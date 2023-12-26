051d379676813f0fca450ac171bc319bffa7f98e
Flrande <flrande.ch@gmail.com>
2023-12-26 16:18:45 +0800

    feat: support compostion

--- src/core/selection/index.ts
+++ src/core/selection/index.ts
@@ -1,4 +1,5 @@
 import invariant from 'tiny-invariant'
+import type { DeltaOperation } from '@blocksuite/store'
 import { getStd } from '../../global'
 import type { BlockElement } from '../../types'
 import { onModelSelectionChange } from './selection-model-change'
@@ -46,6 +47,9 @@ export function bindSelection(editorId: string) {
   const unbindBeforeInput = std.event.add('beforeInput', (ctx) => {
     const event = ctx.get('defaultState').event as InputEvent
     event.preventDefault()
+
+    if (event.isComposing)
+      return
 
     const textSelection = std.selection.find('text')
     if (!textSelection)
@@ -109,9 +113,81 @@ export function bindSelection(editorId: string) {
     })
   })
 
+  let joinDeltas: DeltaOperation[] | null = null
+  const unbindCompositionStart = std.event.add('compositionStart', () => {
+    const textSelection = std.selection.find('text')
+    if (!textSelection)
+      return
+
+    const { to } = textSelection
+    if (!to)
+      return
+
+    const selection = window.getSelection()
+    const range = selection?.getRangeAt(0)
+    if (!range)
+      return
+
+    const blocks = getSelectedBlockElementsByRange(editorId, range, {
+      mode: 'highest',
+    })
+
+    const start = blocks.at(0)
+    const end = blocks.at(-1)
+    if (!start || !end)
+      return
+
+    const startText = start.model.text
+    const endText = end.model.text
+    if (!startText || !endText)
+      return
+
+    requestAnimationFrame(() => {
+      endText.delete(0, to.length)
+      joinDeltas = endText.toDelta()
+
+      std.page.transact(() => {
+        blocks
+          .slice(1)
+          .forEach((block) => {
+            std.page.deleteBlock(block.model)
+          })
+      })
+    })
+  })
+  const unbindCompositionEnd = std.event.add('compositionEnd', (ctx) => {
+    const event = ctx.get('defaultState').event as CompositionEvent
+    event.preventDefault()
+
+    const selection = window.getSelection()
+    const range = selection?.getRangeAt(0)
+    if (!range || !range.collapsed)
+      return
+
+    if (!joinDeltas)
+      return
+
+    const block = range.startContainer.parentElement?.closest<BlockElement>('[data-ab-block]')
+    if (!block)
+      return
+
+    const text = block.model.text
+    if (!text)
+      return
+
+    joinDeltas.unshift({
+      retain: text.length,
+    })
+    text.applyDelta(joinDeltas)
+
+    joinDeltas = null
+  })
+
   return () => {
     unbindSelectionChange()
     unbindBeforeInput()
+    unbindCompositionStart()
+    unbindCompositionEnd()
     modelSelectionChangeDispose.dispose()
   }
 }
